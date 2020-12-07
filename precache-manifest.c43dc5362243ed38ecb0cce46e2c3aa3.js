b38a9552f904920918cc7ee7baff1c7c6205e23b
Jasurbek <jasur1998gopirjonov@gmail.com>
2020-12-07 17:58:58 +0500

    Updates

---
+++ precache-manifest.c43dc5362243ed38ecb0cce46e2c3aa3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3bbcd4339cd9ff51aee96e07ab088c",
+    "url": "/Temp/index.html"
+  },
+  {
+    "revision": "7de5291dcdef87feed0a",
+    "url": "/Temp/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "445af9c007546d325af6",
+    "url": "/Temp/static/css/main.6ee7d5ef.chunk.css"
+  },
+  {
+    "revision": "7de5291dcdef87feed0a",
+    "url": "/Temp/static/js/2.68413fcd.chunk.js"
+  },
+  {
+    "revision": "887da7838c4cad7f3c52965247d148c2",
+    "url": "/Temp/static/js/2.68413fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445af9c007546d325af6",
+    "url": "/Temp/static/js/main.b8639b08.chunk.js"
+  },
+  {
+    "revision": "3b42d79f02190f33e377",
+    "url": "/Temp/static/js/runtime-main.9877ec98.js"
+  },
+  {
+    "revision": "7e0ff0c5aecc457a84606be1c0d756aa",
+    "url": "/Temp/static/media/ablsoft.7e0ff0c5.png"
+  },
+  {
+    "revision": "08113387747e2d8d93f9f2dc41707803",
+    "url": "/Temp/static/media/azamaka.08113387.png"
+  },
+  {
+    "revision": "a2a6262be84ddb13f933c8793ae2142d",
+    "url": "/Temp/static/media/computerImage1.a2a6262b.png"
+  },
+  {
+    "revision": "e5766b89b2c665d5426e8a1331f80f54",
+    "url": "/Temp/static/media/creationofFizmasoftNew.e5766b89.png"
+  },
+  {
+    "revision": "06a1f4db388a959662d4463b0c0a524b",
+    "url": "/Temp/static/media/desktop.06a1f4db.png"
+  },
+  {
+    "revision": "9624c17948b312f95783ad2164241ae1",
+    "url": "/Temp/static/media/faceconNew.9624c179.png"
+  },
+  {
+    "revision": "4b6cdebad2a20ef7bd864dcf9bb110cb",
+    "url": "/Temp/static/media/firstLogoNew.4b6cdeba.png"
+  },
+  {
+    "revision": "2b00f8035c2040d88a0c399dbdaa0f23",
+    "url": "/Temp/static/media/hardware.2b00f803.png"
+  },
+  {
+    "revision": "65b0ef455217c6da4e1b600c1cd4eb2b",
+    "url": "/Temp/static/media/location.65b0ef45.png"
+  },
+  {
+    "revision": "8db1226192733d0d98a4306eb4ed5032",
+    "url": "/Temp/static/media/newLogo.8db12261.JPG"
+  },
+  {
+    "revision": "ddb62fcb7deaebcff14fb4a18400dcac",
+    "url": "/Temp/static/media/ourproject.ddb62fcb.png"
+  },
+  {
+    "revision": "ad5827a6ebb7381951c07e17365f527a",
+    "url": "/Temp/static/media/presentationNew.ad5827a6.png"
+  },
+  {
+    "revision": "81848f1537947ef3de526120dd533b2d",
+    "url": "/Temp/static/media/realeseOfNew.81848f15.png"
+  },
+  {
+    "revision": "7650fa312bdab603aaa22d4f01636a4b",
+    "url": "/Temp/static/media/releaseOfParentNew.7650fa31.png"
+  },
+  {
+    "revision": "9f4b9e304aa1ede5a0293ca9acd02705",
+    "url": "/Temp/static/media/safeCityNew.9f4b9e30.png"
+  },
+  {
+    "revision": "75f0996d11f3bf666614d278755aec0d",
+    "url": "/Temp/static/media/server.75f0996d.png"
+  },
+  {
+    "revision": "0208847aa1a115d8cd82fdd0be79e255",
+    "url": "/Temp/static/media/smartbaselogo.0208847a.png"
+  },
+  {
+    "revision": "b6b2a9caead20f865b5f3e83e4597794",
+    "url": "/Temp/static/media/support3.b6b2a9ca.svg"
+  },
+  {
+    "revision": "92cef8164d9226e60f4ef3f116ef868b",
+    "url": "/Temp/static/media/togglerIcon.92cef816.svg"
+  },
+  {
+    "revision": "b9c002acd686c2dace5979f69b07457c",
+    "url": "/Temp/static/media/twitter.b9c002ac.svg"
+  },
+  {
+    "revision": "2eddc3de8ed4ed4c967af920b68afe52",
+    "url": "/Temp/static/media/uztracking1.2eddc3de.png"
+  },
+  {
+    "revision": "dd8a4b0286c9c9dd30663b2c5e3f6f2f",
+    "url": "/Temp/static/media/weblogo.dd8a4b02.png"
+  }
+]);
